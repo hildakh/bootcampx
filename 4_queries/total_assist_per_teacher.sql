3d3bf2cd03c2c43e67e42af9767a493f51a0645c
hildakh <h_khajehamiri@yahoo.com>
2019-11-13 00:32:29 +0000

    gets the total number of times a teacher assisted a specific cohort

---
+++ 4_queries/total_assist_per_teacher.sql
@@ -0,0 +1,9 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+-- Aliases must be used in group by
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
