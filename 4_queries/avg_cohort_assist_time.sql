ab4e1c74b2e8331018611d3faf29e5f7eb0e9691
hildakh <h_khajehamiri@yahoo.com>
2019-11-13 00:26:05 +0000

    gets the avg assistance time each cohort received

--- 4_queries/avg_cohort_assist_time.sql
+++ 4_queries/avg_cohort_assist_time.sql
@@ -1,6 +1,6 @@
-SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_time
-FROM assistance_requests
-JOIN students ON students.id = student_id
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY average_assistance_time;
