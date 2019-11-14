74a11dc34b87769dd9ed5a80532d8717aad5e45e
hildakh <h_khajehamiri@yahoo.com>
2019-11-14 20:16:45 +0000

    Sanitized/parameterized the code in teacher.js- It takes the exam name of the cohort as node argument and returns the list of the teachers who assisted ss in that cohort

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,9 @@
 const { Pool } = require("pg");
+const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array
+const values = [`${cohortName}`];
+// , limit];
 
 const pool = new Pool({
   user: "vagrant",
@@ -7,18 +12,16 @@ const pool = new Pool({
   database: "bootcampx"
 });
 
-pool
-  .query(
-    `
+pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 `
-  )
+, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
