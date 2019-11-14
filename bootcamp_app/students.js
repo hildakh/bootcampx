c6cec2cc66fad7111423345a24c524a859f5d3fe
hildakh <h_khajehamiri@yahoo.com>
2019-11-14 05:03:11 +0000

    added a pool. query to get students in a cohort

---
+++ bootcamp_app/students.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohort_id, cohorts.name AS cohort
+FROM students
+JOIN cohorts
+ON cohorts.id = cohort_id 
+LIMIT 5;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} had an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  
+  })
+  .catch(err => console.error('query error', err.stack));
